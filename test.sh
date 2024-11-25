cc4ac21d4b0b77db85f1d1ac02295425659bc47f
PlebbyG <plebeiusgaragicus@gmail.com>
2024-11-25 15:27:55 -0800

    tidy

--- test.sh
+++ test.sh
@@ -5,3 +5,4 @@ docker compose -f docker-compose-dev.yml --env-file .env up
 # docker logs -f cloudflared_tunnel
 
 # for i in {1..10}; do docker logs -f open-webui pipelines cloudflared_tunnel watchtower; sleep 1; done
+
